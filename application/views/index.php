58c8a0cd9dacb1fd1c01f1e28d6dd67612829d9a
alfarrabi26 <alfarrabim@gmail.com>
2022-09-12 17:56:25 +0700

    cobain aja dulu

---
+++ application/views/index.php
@@ -0,0 +1,56 @@
+<!--================Home Banner Area =================-->
+<section class="home_banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="container">
+            <div class="banner_content text-center">
+                <h3 data-aos="fade-up" data-aos-duration="1600">Belajar Dimana Saja & Kapan Saja <br /> Mudah Dengan Learnify</h3>
+                <p data-aos="fade-up" data-aos-duration="1900">Dengan Learnify kemudahan kegiatan belajar mengajar dapat terpenuhi. Para guru dan siswa dapat
+                    belajar meski banyak halangan atau rintangan. Nikmati Pembelajaran terstruktur dan efektif menggunakan Learnify serta kemudahan belajar dengan menggunakan aplikasi kami. </p>
+                <a data-aos="fade-up" data-aos-duration="2000" class="main_btn" href="<?= base_url('user/registration') ?>#registration">Buat Avatar Sekarang <span class="lnr lnr-arrow-right"></span></a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ================End Home Banner Area ================= -->
+
+<!--================Finance Area =================-->
+<section class="finance_area">
+    <div class="container">
+        <div class="finance_inner row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="finance_item">
+                    <div data-aos="fade-right" data-aos-duration="1600" class="media">
+                        <div class="d-flex">
+                            <i class="lnr lnr-users"></i>
+                        </div>
+                        <div class="media-body">
+                            <h5>Staff & User Management</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Finance Area =================-->
+
+
+
+<!--================Team Area =================-->
+
+<!--================End Team Area =================-->
+
+<!--================Impress Area =================-->
+<section class="impress_area p_120">
+    <div class="container">
+        <div class="impress_inner text-center">
+            <h2 data-aos="fade-up" data-aos-duration="1800">LOGIN SEBAGAI STAFF SEKARANG</h2>
+            <p data-aos="fade-up" data-aos-duration="2000">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free. Abstract chaos snare play truth ultimate good self. God overcome sexuality pious abstract good decieve revaluation aversion good. Virtues chaos overcome society holiest truth.
+            </p>
+            <a data-aos="fade-up" data-aos-duration="2200" class="main_btn" href="<?= base_url('welcome/guru') ?>">Login Sebagai Staff <span class="lnr lnr-arrow-right text-black"></span></a>
+        </div>
+    </div>
+</section>
+<!--================End Impress Area =================-->
